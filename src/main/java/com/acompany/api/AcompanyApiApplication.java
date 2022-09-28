95cfb9e349ed3bcf7eb018d6ee9e9f963423c54b
Arlon Antonio <arlon20092009@gmail.com>
2022-09-28 18:18:16 -0300

    primeiro commit

---
+++ src/main/java/com/acompany/api/AcompanyApiApplication.java
@@ -0,0 +1,13 @@
+package com.acompany.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcompanyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcompanyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acompany/api/AcompanyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acompany.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcompanyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
